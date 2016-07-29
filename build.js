c429ed25db84c8120494d45d39fad9973805bb3e
Henrik Ingo <henrik.ingo@avoinelama.fi>
2016-07-30 00:01:22 +0300

    Add a goto plugin that consumes the preStepLeave api.

--- build.js
+++ build.js
@@ -5,6 +5,7 @@ buildify()
   .load('src/impress.js')
   .concat(['src/plugins/autoplay/autoplay.js',
            'src/plugins/extras/extras.js',
+//           'src/plugins/goto/goto.js',
            'src/plugins/navigation/navigation.js',
            'src/plugins/navigation-ui/navigation-ui.js',
            'src/plugins/progress/progress.js',
